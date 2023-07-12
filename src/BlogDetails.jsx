bb0ea019db6ced142c0a3172e560bc92095be70b
Elon Thy <115206732+Leap-Chanvuthy@users.noreply.github.com>
2023-07-12 13:50:05 +0700

    finished project

--- src/BlogDetails.jsx
+++ src/BlogDetails.jsx
@@ -1,9 +1,19 @@
-import { useParams } from "react-router-dom";
+import { useParams , useNavigate} from "react-router-dom";
 import useFetch from "./useFetch";
 
 const BlogDetails = () => {
     const {id} = useParams();
     const {data : blog , isPanding , error} = useFetch ('http://localhost:8000/blogs/' + id);
+    const navigate = useNavigate ();
+    const handleDelete = (e) => {
+        e.preventDefault();
+        fetch ('http://localhost:8000/blogs/' + id , {
+            method: 'DELETE'
+        }).then (() =>{
+            console.log ('blog deleted');
+            navigate('/');
+        })
+    }
     return (
         <div className="blog-details">
           { isPanding && <div>Loading...</div> }
@@ -15,6 +25,7 @@ const BlogDetails = () => {
               <div>{ blog.body }</div>
             </article>
           )}
+          <button onClick={handleDelete}>Delete</button>
         </div>
       );
 }
